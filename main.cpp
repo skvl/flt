d5ace4f085cd1304ac18434e863a718f987999c4
Sergey Kovalev <valor@list.ru>
2018-05-01 12:11:17 +0300

    Задача №1: Добавить обработку ошибок соединений

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+/*
+ * TODO #1 Добавить обработку ошибок соединений.
+ *
+ * Если `QObject::connect() = false`, то необходимо вывести предупреждение об
+ * ошибке и прервать выполнение.
+ * Данную возможность стоит вынести в класс.
+ */
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
